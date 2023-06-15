f0457978f4d239202e2ee2b85992956522ed562b
roland444 <123905024+roland444@users.noreply.github.com>
2023-06-15 21:44:25 +0200

    Implemented FileReader

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const _audioFile = document.getElementById("myfile");
-const _playBtn = document.getElementById("play-btn");
+const _audioFile = document.querySelector("#audio-input");
+const _playBtn = document.querySelector("#play-btn");
+const _stopBtn = document.querySelector("#stop-btn");
 
-function playMusic() {
-    let audio = new Audio(_audioFile.value);
-    audio.play();
-    
-    
+const readFile = (files) => {
+    let fileReader = new FileReader();
+        fileReader.readAsArrayBuffer(files[0]);
+
+        _playBtn.addEventListener("click", function(fileReader) {
+            playAudioFile(fileReader);
+        })
+
+        // _stopBtn.addEventListener("click", function() {
+        //     stopAudioFile()
+        // })
+
 }
 
-playMusic();
+const playAudioFile = (file) => {
+    let context = new window.AudioContext();
+        context.decodeAudioData(file, function(buffer) {
+            let source = context.createBufferSource();
+				source.buffer = buffer;
+				source.loop = false;
+				source.connect(context.destination);
+				source.start(0);
+        })
+}
 
-_playBtn.addEventListener("click", playMusic);
+const stopAudioFile = () => {
+
+}
